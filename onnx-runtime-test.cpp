c3813b395e063833cb20a299f501a493953e7dce
AzetaTakuya <azeta.takuya@isid.co.jp>
2024-01-14 15:42:33 +0900

    [update] image read mnist

--- onnx-runtime-test.cpp
+++ onnx-runtime-test.cpp
@@ -1,6 +1,7 @@
-﻿#include <onnxruntime_cxx_api.h>
-#include <algorithm>
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <onnxruntime_cxx_api.h>
+#include <opencv2/opencv.hpp>
 
 struct MNIST {
 	MNIST() {
@@ -28,7 +29,7 @@ struct MNIST {
 
 private:
 	Ort::Env env;
-	Ort::Session session_{ env, L"models/mnist.onnx", Ort::SessionOptions{nullptr} };
+	Ort::Session session_{ env, L"models/mnist.onnx", Ort::SessionOptions{nullptr}};
 
 	Ort::Value input_tensor_{ nullptr };
 	std::array<int64_t, 4> input_shape_{ 1, 1, width_, height_ };
@@ -37,10 +38,35 @@ private:
 	std::array<int64_t, 2> output_shape_{ 1, 10 };
 };
 
-std::unique_ptr<MNIST> mnist_;
+
+// 画像の読み込み/前処理
+std::array<float, 28 * 28> preprocess_image(const std::string& image_path) {
+	// 画像をグレースケールで読み込む
+	cv::Mat img = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+	if (img.empty()) {
+		throw std::runtime_error("画像を読み込めませんでした: " + image_path);
+	}
+	// resize image
+	cv::Mat resized_img;
+	cv::resize(img, resized_img, cv::Size(28, 28));
+	// normalize image
+	cv::Mat img_normalized;
+	resized_img.convertTo(img_normalized, CV_32F, 1.0 / 255.0);
+	// show image
+	cv::imshow("normalized", img_normalized);
+	cv::waitKey(1000);
+	// convert vector
+	std::vector<float> input_tensor_values;
+	input_tensor_values.assign((float*)img_normalized.datastart, (float*)img_normalized.dataend);
+	// convert array
+	std::array<float, 28* 28> input_tensor_array;
+	std::copy(input_tensor_values.begin(), input_tensor_values.end(), input_tensor_array.begin());
+	return input_tensor_array;
+}
 
 int main()
 {
+	std::unique_ptr<MNIST> mnist_;
 	try {
 		mnist_ = std::make_unique<MNIST>();
 	}
@@ -48,39 +74,8 @@ int main()
 		std::cerr << exception.what() << std::endl;
 		return 1;
 	}
-
-	mnist_->input_image_ = {
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	};
+	mnist_->input_image_ = preprocess_image("img/test.png");
 	mnist_->Run();
-	std::cout << mnist_->result_ << std::endl;
-
+	std::cout << "MNIST Result: " << mnist_->result_ << std::endl;
 	return 0;
 }
